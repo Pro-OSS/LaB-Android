fb3f51850684fabe2761cb1faafbca79e21a1127
mwg <627986878@qq.com>
2016-07-28 01:22:40 +0800

    .

---
+++ app/src/main/java/top/lhzbxx/lab_android/BasePresenter.java
@@ -0,0 +1,8 @@
+package top.lhzbxx.lab_android;
+
+/**
+ * Created by Administrator on 2016/7/24.
+ */
+public interface BasePresenter {
+    void start();
+}
---
+++ app/src/main/java/top/lhzbxx/lab_android/BaseView.java
@@ -0,0 +1,9 @@
+package top.lhzbxx.lab_android;
+
+/**
+ * Created by Administrator on 2016/7/24.
+ */
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+
+}
