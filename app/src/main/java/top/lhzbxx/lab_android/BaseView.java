af4bbb9ccbda2f03a0c64e624f997b53b9093ce8
lhzbxx <lhzbxx@gmail.com>
2016-08-29 09:54:58 +0800

    Reinit.

--- app/src/main/java/top/lhzbxx/lab_android/BaseView.java
+++
@@ -1,9 +0,0 @@
-package top.lhzbxx.lab_android;
-
-/**
- * Created by Administrator on 2016/7/24.
- */
-public interface BaseView<T> {
-    void setPresenter(T presenter);
-
-}
--- presentation/src/main/java/top/lhzbxx/lab_android/BasePresenter.java
+++
@@ -1,8 +0,0 @@
-package top.lhzbxx.lab_android;
-
-/**
- * Created by Administrator on 2016/7/24.
- */
-public interface BasePresenter {
-    void start();
-}
